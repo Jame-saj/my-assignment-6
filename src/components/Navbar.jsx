573480cc80d8a665bb7c605343dca29cdfa37379
Jame-saj <sharminakterjame26@gmail.com>
2026-04-01 00:51:06 +0600

    navbar only

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import cartImg from "../assets/products/shopping-cart.png"
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="container mx-auto navbar p-7 items-center">
+        <div className="navbar-start">
+          <a className=" text-4xl font-extrabold bg-gradient-to-r from-purple-800 to-blue-500  bg-clip-text text-transparent">
+            DigiTools
+          </a>
+        </div>
+
+        <div className="navbar-center  sm:hidden md:flex lg:flex">
+          <ul className="menu text-black menu-horizontal font-medium px-1">
+            <li>
+              <a>Products</a>
+            </li>
+
+            <li>
+              <a>Features</a>
+            </li>
+            <li>
+              <a>Pricing</a>
+            </li>
+
+            <li>
+              <a>Testimonials</a>
+            </li>
+            <li>
+              <a>FAQ</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="navbar-end gap-4">
+          <img src={cartImg} alt="" />
+          <button className="text-[16px]">Login</button>
+          <button class="btn btn-primary rounded-full bg-gradient-to-r from-blue-600 to-purple-600">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
